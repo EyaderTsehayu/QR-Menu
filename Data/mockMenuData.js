846a2c49cfbff712a628caafdca53d179cdb7587
Eyader Tsehayu <eyaderts@gmail.com>
2023-07-27 14:08:54 +0300

    add mock menu data

---
+++ Data/mockMenuData.js
@@ -0,0 +1,74 @@
+const menu = [
+  {
+    burger: [
+      { title: "Special", price: "320 ETB", tags: "cheese | chips | myonize" },
+      {
+        title: "Hamburger",
+        price: "320 ETB",
+        tags: "cheese | chips | rice | myonize",
+      },
+      { title: "Beef", price: "320 ETB", tags: "cheese | chips | myonize" },
+    ],
+    pizza: [
+      { title: "Special", price: "320 ETB", tags: "cheese | egg | ketchup" },
+      { title: "Tuna", price: "320 ETB", tags: "cheese | chips | sticky" },
+      { title: "Vegetable", price: "320 ETB", tags: "cheese | chips | sticky" },
+    ],
+    beer: [
+      {
+        title: "Dashen",
+        price: "40 ETB",
+        tags: "Aperol | Ethiopian flavor | 330 ml",
+      },
+      {
+        title: "Waliya",
+        price: "40 ETB",
+        tags: "Cool | Ginger beer | Slice of lime",
+      },
+      {
+        title: "Habesha",
+        price: "40 ETB",
+        tags: "Golden | Gentle | Sugar",
+      },
+      {
+        title: "Bedele",
+        price: "40 ETB",
+        tags: "Pure barley | filterd Brown | Angostura Bitters",
+      },
+      {
+        title: "Giorgis",
+        price: "40 ETB",
+        tags: "Hard | Oldest one | garnish",
+      },
+    ],
+    wine: [
+      {
+        title: "Awash ",
+        price: "440 ETB",
+        tags: "AU | Bottle",
+      },
+      {
+        title: "Axumite",
+        price: "340 ETB",
+        tags: "AU | Bottle",
+      },
+      {
+        title: "Guder",
+        price: "390 ETB",
+        tags: "FR | 750 ml",
+      },
+      {
+        title: "SouthAfrican",
+        price: "1230 ETB",
+        tags: "CA | 750 ml",
+      },
+      {
+        title: "Guinness",
+        price: "330 ETB",
+        tags: "IE | 750 ml",
+      },
+    ],
+  },
+];
+
+export { menu };
